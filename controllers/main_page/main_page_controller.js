54792e8649691d75991fe2607b49474bb06521c8
unknown <dkanavaris@gmail.com>
2021-11-19 19:41:50 +0200

    Fixed problem with projects.json

--- controllers/main_page/main_page_controller.js
+++ controllers/main_page/main_page_controller.js
@@ -13,8 +13,6 @@ exports.main_page_get = function(req, res){
 }
 
 exports.main_page_create_project = async function(req, res){
-    console.log(req.body.project_name);
-    console.log(res.locals.currentUser);
 
     /* Check if user alreay has a project with this name*/
     const exists = await Project.exists({owner: res.locals.currentUser.username,
@@ -36,7 +34,27 @@ exports.main_page_create_project = async function(req, res){
         if(err){
             return next(err);
         }
+
+        /* Create the directory for the project */
         fs.mkdirSync(project_path);
+
+        /*Add the project to user's json file containing shared and owned projects */
+        let obj = {
+            project_name : req.body.project_name,
+            project_owner : res.locals.currentUser.username,
+            project_path : project_path
+        };
+
+        const json_path = res.locals.currentUser.directory + "\\" + "projects.json"
+
+        if(!fs.existsSync(json_path)){
+            fs.writeFileSync(json_path, "[]");
+        }
+
+        let json_file = require(json_path);
+        json_file.push(obj);
+        fs.writeFileSync(json_path, JSON.stringify(json_file));
+
+        res.redirect("/main-page");
     });
-    res.redirect("/main-page");
 }
