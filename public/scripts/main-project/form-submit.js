18cc6749aabfa169b58897591dfe783af2b1e0dd
unknown <dkanavaris@gmail.com>
2021-11-21 15:44:15 +0200

    Added file and folder creation UI in projects

---
+++ public/scripts/main-project/form-submit.js
@@ -0,0 +1,32 @@
+const file_add_button = document.querySelector("#file_plus");
+const folder_add_button = document.querySelector("#folder_plus");
+const myform = document.getElementsByClassName("myform")[0];
+
+
+file_add_button.addEventListener("click", add_file);
+folder_add_button.addEventListener("click", add_folder);
+
+
+function add_file(){
+    myform.style.display = "block"
+    myform.addEventListener('submit', function(s) {
+        s.preventDefault();
+        let text = form_input_name.value;
+        console.log(window.location.href + "/file_create/" + text);
+        this.action = window.location.href + "/file_create/" + text;
+        this.submit();
+        myform.style.display = "none";
+    });
+}
+
+function add_folder(){
+    myform.style.display = "block"
+    myform.addEventListener('submit', function(s) {
+        s.preventDefault();
+        let text = form_input_name.value;
+        console.log(window.location.href + "/folder_create/" + text);
+        this.action = window.location.href + "/folder_create/" + text;
+        this.submit();
+        myform.style.display = "none";
+    });
+}
--- routes/main-page-route/main-page.js
+++ routes/main-page-route/main-page.js
@@ -3,15 +3,22 @@ const express = require('express');
 const  main_page_controller = require('../../controllers/main_page/main_page_controller');
 const router = express.Router();
 
+
+router.post("/:username/:project_name/file_create/:filename", function(req, res){
+
+    console.log("Will create file: " + req.params.filename);
+    res.redirect("back");
+});
+
+router.post("/:username/:project_name/folder_create/:foldername", function(req, res){
+
+    console.log("Will create folder: " + req.params.foldername);
+    res.redirect("back");
+});
+
 router.get("/:username/:project_name", function(req, res){
 
     res.render("main-page-project", {project_name: req.params.project_name});
-})
-
-
-router.get('/update', function(req, res) {
-    console.log("Got update request");
-    return;
 });
 
 /* GET users listing. */
